d5ad274b760d455ef9adf800eb2be123861422c8
Olivier Vermeulen <oliviervermeulen35@gmail.com>
2018-05-21 23:22:24 +0200

    split CSV into 2D array

--- 2018_JavaOO_Project_zonneschijn - startpunt-studenten/src/presentatie/JavaOO_dl2/TestIO.java
+++ 2018_JavaOO_Project_zonneschijn - startpunt-studenten/src/presentatie/JavaOO_dl2/TestIO.java
@@ -31,21 +31,26 @@ public class TestIO {
             System.out.println("error");
         }
          */
-        
+
         try {
-            String filePath = "C:/Users/Olivier PC/Dropbox/2e semester/Java (OOP)/Project/2018_JavaOO_Project_zonneschijn - startpunt-studenten/data/Metingen 2017-12/Energie_en_vermogen_Dag_2017-12-10.csv";            
+            String filePath = "C:/Users/Olivier PC/Dropbox/2e semester/Java (OOP)/Project/2018_JavaOO_Project_zonneschijn - startpunt-studenten/data/Metingen 2017-12/Energie_en_vermogen_Dag_2017-12-10.csv";
             String[] file = TextFile.readLines(filePath);
-            String[][] timeAndMeasurements = new String[file.length][]; 
-            for (int i = 0; i < timeAndMeasurements.length; i++) {
-                timeAndMeasurements[i] = file[i].split(";");               
+            for (int i = 0; i < file.length; i++) {
+                file[i] = file[i].replaceAll(",", ".");
+                
             }
-            for (int i = 0; i < timeAndMeasurements.length; i++) {
-                for (int j = 0; j < timeAndMeasurements[i].length; j++) {
-                    System.out.print(timeAndMeasurements[i][j]);
+            String[][] files = new String[file.length][];
+            for (int i = 0; i < file.length; i++) {
+                files[i] = file[i].split(";");
+            }
+
+            for (int i = 1; i < files.length; i++) {
+                if (files[i].length == 2 && Double.parseDouble(files[i][1]) > 0) {
+                    System.out.print(files[i][0] + " - ");
+                    System.out.println(files[i][1]);
                 }
-                System.out.println("");
             }
-            
+
         } catch (IOException io) {
             System.out.println("error");
         }
