8e6ed8a98c81c042a0eac61a2e3f3a0bbaf578eb
Olivier Vermeulen <oliviervermeulen35@gmail.com>
2018-05-21 22:17:19 +0200

    reading file in testIO

--- 2018_JavaOO_Project_zonneschijn - startpunt-studenten/src/presentatie/JavaOO_dl2/TestIO.java
+++ 2018_JavaOO_Project_zonneschijn - startpunt-studenten/src/presentatie/JavaOO_dl2/TestIO.java
@@ -31,6 +31,17 @@ public class TestIO {
             System.out.println("error");
         }
          */
+        
+        try {
+            String bestand = "C:/Users/Olivier PC/Dropbox/2e semester/Java (OOP)/Project/2018_JavaOO_Project_zonneschijn - startpunt-studenten/data/Metingen 2017-12/Energie_en_vermogen_Dag_2017-12-10.csv";            
+            String allInFile = TextFile.read(bestand); // Reads everything as one string.
+            String[] splitFile = allInFile.split(";");
+            for (int i = 0; i < splitFile.length; i++) {
+                System.out.println(splitFile[i]);                
+            }
+        } catch (IOException io) {
+            System.out.println("error");
+        }
     }
 
 }
