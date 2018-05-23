5bd926555ddc473f96fb38cf6b9184e2bc87a10e
Olivier Vermeulen <oliviervermeulen35@gmail.com>
2018-05-23 20:05:20 +0200

    Added CSV files

--- Project_Zonneschijn/src/presentatie/JavaOO_dl4/Panel_2Days.java
+++ Project_Zonneschijn/src/presentatie/JavaOO_dl4/Panel_2Days.java
@@ -1,5 +1,6 @@
-package presentatie.JavaOO_dl2;
+package presentatie.JavaOO_dl4;
 
+import presentatie.JavaOO_dl3.*;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.util.ArrayList;
@@ -9,15 +10,18 @@ import javax.swing.BorderFactory;
  *
  * @author Olivier PC
  */
-public class Panel_1Day extends javax.swing.JPanel {
+public class Panel_2Days extends javax.swing.JPanel {
 
     private ArrayList<Double> measurements;
     private Graphics g;
+    private double maximum;
+    private int width;
+    private int height;
 
     /**
      * Creates new form Panel_1Day
      */
-    public Panel_1Day() {
+    public Panel_2Days() {
         initComponents();
         this.setBorder(BorderFactory.createLineBorder(Color.black));
         this.measurements = new ArrayList<>();
@@ -28,25 +32,38 @@ public class Panel_1Day extends javax.swing.JPanel {
         super.paintComponent(g);
         this.g = g;
         this.g.setColor(Color.BLACK);
+        this.drawAxis();
         this.drawGraph();
         this.repaint();
     }
 
     private void drawGraph() {
         if (!this.measurements.isEmpty()) {
-            int width = this.getWidth();
-            int height = this.getHeight();
+            this.width = this.getWidth();
+            this.height = this.getHeight();
             for (int i = 0; i < this.measurements.size(); i++) { // for loop going over all the measurements and plotting them on the graph.
-                g.drawOval(i * 5, height - 2 - (int) (height * this.measurements.get(i) / 4), 4, 4);
+                this.g.drawOval(i * 5, this.height - 2 - (int) (this.height * this.measurements.get(i) / 4), 4, 4);
                 if (i >= 1) {
                     int firstX = i * 5 - 3;
                     int secondX = i * 5 + 2;
-                    int firstY = height - (int) (height * this.measurements.get(i - 1) / 4);
-                    int secondY = height - (int) (height * this.measurements.get(i) / 4);
-                    g.drawLine(firstX, firstY, secondX, secondY);
+                    int firstY = this.height - (int) (this.height * this.measurements.get(i - 1) / 4);
+                    int secondY = this.height - (int) (this.height * this.measurements.get(i) / 4);
+                    this.g.drawLine(firstX, firstY, secondX, secondY);
                 }
             }
             this.repaint();
+        }
+    }
+
+    private void drawAxis() {
+        if (!this.measurements.isEmpty()) {
+            for (int i = 0; i <= this.width; i += 60) {
+                this.g.drawLine(i, 0, i, this.height);
+            }
+            for (int i = 0; i <= this.height; i += 60) {
+                this.g.drawLine(0, i, this.width, i);
+                
+            }
         }
     }
 
@@ -68,6 +85,10 @@ public class Panel_1Day extends javax.swing.JPanel {
         }
     }
 
+    public void setMaximum(double maximum) {
+        this.maximum = maximum;
+    }
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -84,13 +105,17 @@ public class Panel_1Day extends javax.swing.JPanel {
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 480, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 300, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
+
+    void ffillMeasurements(String[][] fillCustomData) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
